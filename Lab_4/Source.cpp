da92d0bd11db5a9f5f3fb39715f9262b66d6453c
BelskiMikolai <146476748+BelskiMikolai@users.noreply.github.com>
2023-10-16 13:00:51 +0300

    Create lab 4

---
+++ Lab_4/Source.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+int main()
+{
+	//Создаём переменные
+	double y[10];
+	double sum{ 0 };
+	int idlLast{ -1 };
+	bool isBetween{ false };
+
+	//Зпрашиваем значения массива, находим индекс последнего нулевого элемента
+	std::cout << "Enter y:\n";
+	for (int i = 0; i < 10; i++)
+	{
+		int num{};
+		std::cin >> num;
+		y[i] = num;
+
+		if (num == 0)
+		{
+			idlLast = i;
+		}
+	}
+
+	// Находим сумму от первого нулевого до последнего, используя его индекс
+	for (int i = 0; i < 10; i++)
+	{
+		if (isBetween && i < idlLast)
+		{
+			sum += y[i];
+		}
+		if (y[i] == 0 && !isBetween)
+		{
+			isBetween = true;
+		}
+	}
+
+	//Выводим сумму
+	std::cout << "Sum: " << sum;
+
+	return 0;
+}
