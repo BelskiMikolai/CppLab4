820f8f4ff2da0de5016426b3d869066ea0e81cd1
BelskiMikolai <146476748+BelskiMikolai@users.noreply.github.com>
2023-10-16 12:51:55 +0300

    Copy

---
+++ Lab4_2/Source.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+int main()
+{
+	//Создаём переменные
+	double y[10];
+	double y_max{ 0 };
+	double mult{ 1 };
+	bool isBetween{ false };
+
+	//Зпрашиваем значения массива, находим максимальный элемент
+	std::cout << "Enter y:\n";
+	for (int i = 0; i < 10; i++)
+	{
+		int num{};
+		std::cin >> num;
+		y[i] = num;
+		if (i == 0)
+		{
+			y_max = num;
+		}
+		if (num > y_max)
+		{
+			y_max = num;
+		}
+	}
+
+	//Считаем произведение элементов масива после наибольшего элемента
+	for (int i = 0; i < 10; i++)
+	{
+		if (isBetween)
+		{
+			mult *= y[i];
+		}
+		if (y[i] == y_max && !isBetween)
+		{
+			isBetween = true;
+		}
+	}
+
+	//Выводем произведение
+	std::cout << "Mult: " << mult;
+
+	return 0;
+}
